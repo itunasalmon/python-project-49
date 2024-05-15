0672ca5c44a5113b7546c64fa43f466e8ccfdbb3
Tuna Salmon <tunasalmon@yandex.ru>
2024-05-15 23:47:09 +0400

    added "brain-even" game

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/itunasalmon/python-project-49/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/itunasalmon/python-project-49/actions)
 [![Maintainability](https://api.codeclimate.com/v1/badges/098a93a1c04b1b826c44/maintainability)](https://codeclimate.com/github/itunasalmon/python-project-49/maintainability)
+[![asciicast](https://asciinema.org/a/gP2waCQyB9uEOoe7wrg7VKoqs.svg)](https://asciinema.org/a/gP2waCQyB9uEOoe7wrg7VKoqs)
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# file <brain_even.py>
+
+from random import randint
+import prompt
+
+
+def main():
+    print("Welcome to the Brain Games!")
+    user_name = prompt.string('May I have your name? ')
+    print(f'Hello, {user_name}!')
+    
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+
+    round_count = 1
+    min_number = 1
+    max_number = 1000
+
+
+    while round_count <= 3:
+        question = randint(min_number, max_number)
+        print(f'Question: {question}')
+        user_answer = prompt.string('Your answer: ')
+        if question % 2 == 0:
+            correct_answer = 'yes'
+        else:
+            correct_answer = 'no'
+        if not user_answer == correct_answer:
+            print(f"'{user_answer}' is wrong answer ;(. "
+                f"Correct answer was '{correct_answer}'.\n"
+                f"Let's try again, {user_name}!")
+        else:
+            print('Correct!')
+            round_count += 1
+    print(f'Congratulations, {user_name}!')
